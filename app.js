07bbdcb99d158449febb818e05a43927b4723a5e
Lucho <luisparr14@gmail.com>
2022-01-21 10:28:53 -0500

    Config Database and clean code

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const app = express()
 
 const routes = require('./routes/index')
 const connection = require('./database/database')
-connection.on('connection', (err) => {
-  console.log('connected to database')
-})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
-app.use('/', require('./routes/index'))
+
 app.use('/', routes)
+
 module.exports = app
--- database/database.js
+++ database/database.js
@@ -1,23 +1,18 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.PROD_DB_HOST,
+  user: process.env.PROD_DB_USER,
+  password: process.env.PROD_DB_PASS,
+  database: process.env.PROD_DB_NAME
 })
-console.log('DATABASE NAME', connection.config.database)
+
 connection.connect(function (err) {
   if (err) {
     console.error('error connecting: ' + err.stack)
     return
   }
   console.log('Database connected')
-})
-
-connection.query('SELECT * FROM empleados', function (error, results, fields) {
-  if (error) throw error
-  console.log('The solution is: ', results)
 })
 
 module.exports = connection
